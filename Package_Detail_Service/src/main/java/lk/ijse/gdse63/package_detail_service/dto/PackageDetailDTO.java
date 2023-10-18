281fc6da62a9ce7d34c067d9b6af7d353022ce18
Your Name <you@example.com>
2023-10-18 18:31:05 +0530

    update package detail

--- Package_Detail_Service/src/main/java/lk/ijse/gdse63/package_detail_service/dto/PackageDetailDTO.java
+++ Package_Detail_Service/src/main/java/lk/ijse/gdse63/package_detail_service/dto/PackageDetailDTO.java
@@ -12,4 +12,18 @@ import java.io.Serializable;
 @Data
 public class PackageDetailDTO implements Serializable,SuperDTO {
     private String packagedetailId;
-}
+    private String packageCategory;
+    private String travelDuration;
+    private String travelArea;
+    private int noOfAdults;
+    private int noOfChildren;
+    private int totalHeadCount;
+    private boolean withPetsOrNot;
+    private boolean isGuideIncluded;
+    private double packageValue;
+    private double paidValue;
+    private String vehicleId;
+    private String hotelId;
+    private String guideId;
+    private String packageId;
+    private String userId;}
